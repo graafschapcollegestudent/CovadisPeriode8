b91e8700d020eccfc81cae41e8ee2c5cdeec0a9f
transgerda <info@martijnvanboven.nl>
2026-05-12 12:55:12 +0200

    team endpoints

--- Covadis.Api/Application/Interfaces/ITeamService.cs
+++ Covadis.Api/Application/Interfaces/ITeamService.cs
@@ -1,6 +1,9 @@
+using Covadis.Api.Application.DTOs.Team;
+
 namespace Covadis.Api.Application.Interfaces;
 
 public interface ITeamService
 {
-    
+    Task<List<TeamReadDto>?> GetAllTeamsAsync();
+    Task<TeamReadDto?> GetTeamByIdAsync(Guid id);
 }
