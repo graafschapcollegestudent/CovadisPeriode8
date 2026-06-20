bce6214310dd32a1811787f2d33edaf39bb874f0
Hadi <167310042+graafschapcollegestudent@users.noreply.github.com>
2026-06-20 23:21:52 +0200

    taken aanmaken voor managers

---
+++ Covadis.Frontend/DTOs/CreateTaskDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Covadis.Frontend.DTOs;
+
+public class CreateTaskDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime DueDate { get; set; } = DateTime.Now;
+    public int Status { get; set; }
+    public Guid TeamId { get; set; }
+}
